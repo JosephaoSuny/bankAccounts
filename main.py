58bdda2ab90f76fc3f4694b2177b6a15dff75822
JosephaoSuny <oliverja@sunypoly.edu>
2024-09-19 10:39:15 -0400

    Scale to multiple threads

--- main.py
+++ main.py
@@ -1,4 +1,6 @@
 from threading import Lock, Thread
+from typing import List
+import random
 
 class BankAccount:
     def __init__(self, initial_balance: int):
@@ -6,11 +8,9 @@ class BankAccount:
         self.mutex = Lock()
             
     def withdraw(self, amount: int) -> int:
-        print("Calling withdraw, amount = ", amount)
         self.mutex.acquire(blocking=True)
 
         if amount > self.initial_balance:
-            print("Amount too great!")
             raise Exception("Withdraw would overdraft")
 
         self.initial_balance -= amount
@@ -24,12 +24,33 @@ class BankAccount:
         self.initial_balance += amount
         self.mutex.release()
         return amount
-    
-PhilsBankAccount = BankAccount(10)
-    
-t = Thread(target=PhilsBankAccount.withdraw, args=[11])
-t.start()
 
-t.join()
+accounts: List[BankAccount] = list()
+threads: List[Thread] = list()
 
-print(PhilsBankAccount.initial_balance)
+max_accounts = random.randint(10, 100)
+
+for i in range(max_accounts):
+    accounts.append(BankAccount(random.randint(5, 500)))
+
+for i in range(max_accounts):
+    account: BankAccount = accounts[i]
+    
+    for x in range(0, random.randint(4, 20)):
+        def thread_func(function, int, bool): 
+            try:
+                function_name = "Withdrawing" if bool else "Depositing"
+                function(int)
+                print(f"{function_name} {int} dollars")
+            except Exception as over_draft:
+                print(over_draft)
+
+        withdrawl = x % 2 == 0
+        function = account.withdraw if x % 2 == 0 else account.deposit
+        thread = Thread(target=thread_func, args=[function, random.randint(0, 750), withdrawl])
+        thread.start()
+        threads.append(thread)
+
+
+for thread in threads:
+    thread.join()
